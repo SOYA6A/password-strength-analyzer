5c7cc910c77a39743fe1d85e63224aec4f43da35
Benlahcen <benlahcen.assia@gmail.com>
2026-01-07 22:30:21 +0100

    presentation plus debut de fonction analyzer password

---
+++ password_analyzer.py
@@ -0,0 +1,13 @@
+"""
+Password Strength Analyzer
+Auteur: ME SOYA6A
+Description: Analyse la force d'un mot de passe et donne des conseils
+"""
+
+import sys  # Pour récupérer ce que l'utilisateur tape en ligne de commande
+
+def analyser_password(password):
+    """
+    Cette fonction analyse un mot de passe et lui donne un score
+    """
+    
